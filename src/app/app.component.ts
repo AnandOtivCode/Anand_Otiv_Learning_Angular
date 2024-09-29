83443ce4ea75476c030955e435252e47b6473b21
Anand Otiv <w0858857@myscc.ca>
2024-09-28 21:26:28 -0400

    #Generated contentList and contentListItem.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {User} from "./sharedModels/user";
+import {ContentListComponent} from "./content-list/content-list.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
