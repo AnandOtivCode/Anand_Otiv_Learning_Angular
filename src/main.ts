b449b4cd17ae7dd6ca021552284231e8e7b4e13e
“Anand <w0858857@myscc.ca>
2024-10-21 15:01:51 -0400

    Defined a list of routes in my component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import {ContentListItemComponent} from "./app/content-list-item/content-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+const routes: Routes = [
+  {path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
+  { path: 'students', component: ContentListItemComponent},
+  { path: 'students/:id', component: DeveloperDetailComponent },
+  {path:'modify-student', component: ModifyDeveloperComponent},
+  {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
+
