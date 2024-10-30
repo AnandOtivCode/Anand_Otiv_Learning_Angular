7b13c6f2b3c200a15a3f59aff6c8142ea440692c
“Anand <w0858857@myscc.ca>
2024-10-30 06:52:37 -0400

    - Fixed Errors with routing - Changed listView to table rows Finished Bonus by having routing to individual items

---
+++ src/app/Services/developer.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+//import data
+import {userList} from "../Shared/mockDeveloper.data"
+import { Observable, of } from "rxjs";
+import {User} from "../Shared/Models/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeveloperService {
+private developers: User[] = userList; //Local copy of users for CRUD Operations
+
+  constructor() { }
+
+  //Returns all developers
+  getDevelopers(): Observable<User[]>{
+    return of(userList);
+  }
+
+  //Adding basic CRUD methods
+  //Read
+  getDeveloperById(developerID: number): Observable<User | undefined> {
+    const developer = this.developers.find(user => user.id === developerID);
+    return of(developer);
+  }
+
+
+  //Create: Add User
+  addDeveloper(newDeveloper:User) : Observable<User[]>{
+    this.developers.push(newDeveloper)
+    return of(this.developers);
+  }
+
+  //Update an Existing user
+  updateDeveloper(updatedDeveloper: User): Observable<User[]> {
+    const index = this.developers.findIndex(user => user.id === updatedDeveloper.id);
+    if (index !== -1) {
+      this.developers[index] = updatedDeveloper;
+    }
+    return of(this.developers);
+  }
+
+
+  //Delete: Remove a user by ID
+  deleteDeveloper(developerID: number): Observable<User[]> {
+    this.developers = this.developers.filter(user => user.id !== developerID);
+    return of(this.developers);
+  }
+
+
+
+}
